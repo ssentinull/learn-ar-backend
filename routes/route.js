00f2c1f47561456fa825dea00824588fb068c028
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-03 17:48:08 +0700

    feat: separate routes from main.js

---
+++ routes/route.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller');
+
+const router = express.Router();
+
+router.get('/test', userController.test);
+router.post('/user', userController.create);
+router.get('/user/:id', userController.read);
+router.post('/login', authController.login);
+
+module.exports = router;
