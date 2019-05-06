c5b345b421f7c682e9b728b1c4f9c6e19526d978
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-06 16:51:06 +0700

    feat: treasure model

---
+++ models/treasure.model.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const treasureSchema = new Schema({
+  name: String,
+  description: String,
+});
+
+module.exports = mongoose.model('Treasure', treasureSchema);
