d5d18f32e6db8b338889ea0d68b6b8b29b80ee3a
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-07 00:24:05 +0700

    feat: push new treasure data to user attribute

--- routes.js
+++ routes.js
@@ -7,8 +7,9 @@ const router = express.Router();
 
 router.get('/test', userController.test);
 router.get('/init', initController.init);
+router.post('/login', authController.login);
 router.post('/user', userController.createUser);
 router.get('/user/:id', userController.readUser);
-router.post('/login', authController.login);
+router.post('/user/:userId/treasure/:treasureId', userController.pushTreasure);
 
 module.exports = router;
