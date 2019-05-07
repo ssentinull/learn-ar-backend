08b5c1e2a1a824baeba452dcb1d1f4b87b047542
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-07 11:20:21 +0700

    docs: endpoints explanation

--- routes.js
+++ routes.js
@@ -6,12 +6,25 @@ const userController = require('./controllers/user.controller');
 
 const router = express.Router();
 
+// Endpoint to test if the api is working or not 
 router.get('/test', userController.test);
+
+// Endpoint to seed treasure data
 router.get('/init', initController.init);
+
+// Endpoint to login using email and password
 router.post('/login', authController.login);
+
+// Endpoint to create new user with name, email, password as input data
 router.post('/user', userController.createUser);
+
+// Endpoint to get user data based on user id
 router.get('/user/:id', userController.readUser);
+
+// Endpoint to push a new treasure based on treasure id to a user based on user id
 router.post('/user/:userId/treasure/:treasureId', treasureController.pushTreasure);
+
+// Endpoint to get all treasures available
 router.get('/treasures', treasureController.readTreasures);
 
 module.exports = router;
