599b5e21c58bd2cbe9a0dd1576fdff427703d74d
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-11 10:54:14 +0700

    feat: implement token model when creating new user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,16 +1,17 @@
 const crypto = require('crypto');
+const TokenModel = require('../models/token.model');
 const UserModel = require('../models/user.model');
 
 const createUser = (req, res) => {
-  const md5 = crypto.createHash('md5');
-  md5.update((new Date()).getTime() + '');
-
+  const randomNumber = Math.random().toString();
+  const currentDate = (new Date()).valueOf().toString();
+  const sha1 = crypto.createHash('sha1').update(currentDate + randomNumber);
+  
   try{
     const newUser = new UserModel({
       name: req.body.name,
       email: req.body.email,
       password: req.body.password,
-      token: md5.digest('hex'),
     });
 
     newUser.save((err, user) => {
@@ -19,8 +20,15 @@ const createUser = (req, res) => {
         return res.status(400).json({error: err.message});
       }
 
-      return res.status(200).header('Authorization', newUser.token).json(newUser);
+      const newToken = new TokenModel({
+        value: sha1.digest('hex'),
+      });
+
+      newToken.save();
+
+      return res.status(200).header('Authorization', newToken.value).json(user);
     });
+
   } catch (err) {
     
     console.log('Error: ' + err);
@@ -40,6 +48,7 @@ const readUser = (req, res) => {
 
       return res.status(200).json(user);
     });
+
   } catch (err) {
     console.log('Error: ' + err);
     return res.status(400).json({error: err.message});
