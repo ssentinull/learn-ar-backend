587a56b259f519e7a80df5fb41d1a8c04af71fa2
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-11 13:23:09 +0700

    feat: isUnlocked attribute to treasure model

--- models/treasure.model.js
+++ models/treasure.model.js
@@ -4,7 +4,11 @@ const Schema = mongoose.Schema;
 const treasureSchema = new Schema({
   name: String,
   description: String,
-  url: String
+  url: String,
+  isUnlocked: {
+    type:Boolean,
+    default: false
+  }
 });
 
 module.exports = mongoose.model('Treasure', treasureSchema);
