d3733564710132a6a5700f444da02ba0e34f66e7
IbnuAhsani <ibnu.muhari@gmail.com>
2019-05-19 12:10:05 +0700

    feat: global error handling middleware

--- main.js
+++ main.js
@@ -23,6 +23,21 @@ app.use(cors());
 app.use(morgan('combined', {stream: winston.stream}));
 
 app.use('/api', routes);
+app.use((err, req, res, next) => {
+
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // add this line to include winston logging
+  winston.error(`${err.status || 404} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  // render the error page
+  res.status(err.status || 404);
+  res.json({
+    message: err.message,
+  });
+});
 
 app.listen(config.PORT, () => {
   console.log(`starting ${config.ENV} server at http://localhost:${config.PORT}`);
