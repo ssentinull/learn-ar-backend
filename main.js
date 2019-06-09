8ea7d30a527d985adb9e0168667fe7a8bc753e12
IbnuAhsani <ibnu.muhari@gmail.com>
2019-06-09 19:34:53 +0700

    refactor: port .env variable name to match heroku standard

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const routes = require('./routes');
 const winston = require('./config/winston');
 
 const config = {
-  PORT: process.env.APP_PORT || '9000',
+  PORT: process.env.PORT || '9000',
   ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'mongodb://localhost:27017/learn-ar',
 }
